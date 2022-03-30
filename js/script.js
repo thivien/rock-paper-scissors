d13df5610a2e1a4ec517bb6a6c49e6fdc3c9dc84
David <david.kasan32@gmail.com>
2022-03-30 21:14:36 +0200

    Finish adding UI to RPS project and finish writing code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// selecting HTML elements
+const plScore = document.querySelector(".player-score");
+const pcScore = document.querySelector(".computer-score");
+const gameResult = document.querySelector(".game-result");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const lizard = document.querySelector("#lizard");
+const spock = document.querySelector("#spock");
+const resetBtn = document.createElement("div");
+
+// player name
+// const playerName = document.querySelector(".player-name");
+// playerName.textContent = prompt("Enter your name:");
+
 // Function, that returns random computer move
 function computerPlay() {
   const computerMoves = ["rock", "paper", "scissors", "lizard", "spock"];
@@ -11,140 +26,138 @@ let computerScore = 0;
 
 // Function, that defines game's logic
 function playRound(playerSelection, computerSelection) {
-  // IF statement, that checks if player input is valid
-  if (
-    playerSelection !== "rock" &&
-    playerSelection !== "paper" &&
-    playerSelection !== "scissors" &&
-    playerSelection !== "lizard" &&
-    playerSelection !== "spock"
-  ) {
-    return `Please enter valid input. Valid inputs are "rock", "paper", "scissors", "lizard" or "spock" without quotation marks.`;
-  }
-
-  // TIE
-  // if (playerSelection === computerSelection) {
-  //     return `It is a tie! No one gets a point`;
-  // }
-
-  // All combination when player wins
-  // if (playerSelection === "rock" && computerSelection === "scissors" ||
-  //     playerSelection === "rock" && computerSelection === "lizard" ||
-  //     playerSelection === "paper" && computerSelection === "rock" ||
-  //     playerSelection === "paper" && computerSelection === "spock" ||
-  //     playerSelection === "scissors" && computerSelection === "paper" ||
-  //     playerSelection === "scissors" && computerSelection === "lizard" ||
-  //     playerSelection === "lizard" && computerSelection === "paper" ||
-  //     playerSelection === "lizard" && computerSelection === "spock" ||
-  //     playerSelection === "spock" && computerSelection === "rock" ||
-  //     playerSelection === "spock" && computerSelection === "scissors") {
-  //     return `You won, your score is now: ${++playerScore}`;
-  // }
-
-  //All combination when player looses, i.e. computer wins
-  // if (playerSelection === "rock" && computerSelection === "paper" ||
-  //     playerSelection === "rock" && computerSelection === "spock" ||
-  //     playerSelection === "paper" && computerSelection === "scissors" ||
-  //     playerSelection === "paper" && computerSelection === "lizard" ||
-  //     playerSelection === "scissors" && computerSelection === "rock" ||
-  //     playerSelection === "scissors" && computerSelection === "spock" ||
-  //     playerSelection === "lizard" && computerSelection === "rock" ||
-  //     playerSelection === "lizard" && computerSelection === "scissors" ||
-  //     playerSelection === "spock" && computerSelection === "paper" ||
-  //     playerSelection === "spock" && computerSelection === "lizard") {
-  //     return `Computer won, its score is now: ${++computerScore}`;
-  // }
-
   // Player chooses rock
-  if (playerSelection === "rock" && computerSelection === "rock") {
-    return `It's a tie, no one gets a point!`;
+  if (playerSelection === computerSelection) {
+    gameResult.textContent = `It's a tie, no one gets a point!`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return `Computer won! Paper covers rock. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Paper covers rock.`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `You won! Rock crushes scissors. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Rock crushes scissors.`;
   } else if (playerSelection === "rock" && computerSelection === "lizard") {
-    return `You won! Rock crushes lizard. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Rock crushes lizard.`;
   } else if (playerSelection === "rock" && computerSelection === "spock") {
-    return `Computer won! Spock vaporizes rock. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Spock vaporizes rock.`;
   }
 
   // Player chooses paper
   if (playerSelection === "paper" && computerSelection === "rock") {
-    return `You won! Paper covers rock. Your score is now ${++playerScore}.`;
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return `It's a tie, no one gets a point.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Paper covers rock.`;
+  } else if (playerSelection === computerSelection) {
+    gameResult.textContent = `It's a tie, no one gets a point.`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return `Computer won! Scissors cuts paper. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Scissors cuts paper.`;
   } else if (playerSelection === "paper" && computerSelection === "lizard") {
-    return `Computer won! Lizard eats paper. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Lizard eats paper.`;
   } else if (playerSelection === "paper" && computerSelection === "spock") {
-    return `You won! Paper disproves Spock. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Paper disproves Spock.`;
   }
 
   // Player chooses scissors
   if (playerSelection === "scissors" && computerSelection === "rock") {
-    return `Computer won! Rock crushes scissors. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Rock crushes scissors.`;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return `You won! Scissors cuts paper. Your score is now ${++playerScore}.`;
-  } else if (
-    playerSelection === "scissors" &&
-    computerSelection === "scissors"
-  ) {
-    return `It's a tie, no one gets a point.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Scissors cuts paper.`;
+  } else if (playerSelection === computerSelection) {
+    gameResult.textContent = `It's a tie, no one gets a point.`;
   } else if (playerSelection === "scissors" && computerSelection === "lizard") {
-    return `You won! Scissors decapitates lizard. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Scissors decapitates lizard.`;
   } else if (playerSelection === "scissors" && computerSelection === "spock") {
-    return `Computer won! Spock smashes scissors. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Spock smashes scissors.`;
   }
 
   // Player chooses lizard
   if (playerSelection === "lizard" && computerSelection === "rock") {
-    return `Computer won! Rock crushes lizard. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Rock crushes lizard.`;
   } else if (playerSelection === "lizard" && computerSelection === "paper") {
-    return `You won! Lizard eats paper. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Lizard eats paper.`;
   } else if (playerSelection === "lizard" && computerSelection === "scissors") {
-    return `Computer won. Scissors decapitates lizard. Its score is now ${++computerScore}.`;
-  } else if (playerSelection === "lizard" && computerSelection === "lizard") {
-    return `It's a tie, no one gets a point`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won. Scissors decapitates lizard.`;
+  } else if (playerSelection === computerSelection) {
+    gameResult.textContent = `It's a tie, no one gets a point`;
   } else if (playerSelection === "lizard" && computerSelection === "spock") {
-    return `You won! Lizard poisons Spock. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Lizard poisons Spock.`;
   }
 
   // Player chooses Spock
   if (playerSelection === "spock" && computerSelection === "rock") {
-    return `You won! Spock vaporizes rock. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Spock vaporizes rock.`;
   } else if (playerSelection === "spock" && computerSelection === "paper") {
-    return `Computer won! Paper disproves Spock. Its score is now ${++computerScore}.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Paper disproves Spock.`;
   } else if (playerSelection === "spock" && computerSelection === "scissors") {
-    return `You won! Spock smashes scissors. Your score is now ${++playerScore}.`;
+    ++playerScore;
+    plScore.textContent = playerScore;
+    gameResult.textContent = `You won! Spock smashes scissors.`;
   } else if (playerSelection === "spock" && computerSelection === "lizard") {
-    return `Computer won! Lizard poisons Spock. Its score is now ${++computerScore}.`;
-  } else if (playerSelection === "spock" && computerSelection === "spock") {
-    return `It's a tie, no one gets a point.`;
+    ++computerScore;
+    pcScore.textContent = computerScore;
+    gameResult.textContent = `Computer won! Lizard poisons Spock.`;
+  } else if (playerSelection === computerSelection) {
+    gameResult.textContent = `It's a tie, no one gets a point.`;
+  }
+
+  determineWinner();
+}
+
+rock.addEventListener("click", () => playRound("rock", computerPlay()));
+paper.addEventListener("click", () => playRound("paper", computerPlay()));
+scissors.addEventListener("click", () => playRound("scissors", computerPlay()));
+lizard.addEventListener("click", () => playRound("lizard", computerPlay()));
+spock.addEventListener("click", () => playRound("spock", computerPlay()));
+
+function determineWinner() {
+  if (playerScore === 5) {
+    gameResult.textContent = `YOU DID IT! You have bested the coputer. Congratulations!`;
+    resetBtn.style.cssText =
+      "background-color: #001845; color: #f8f8f8;text-transform: uppercase; padding: 0.5rem 1.6rem; border-radius: 0.5rem; cursor: pointer; font-weight: 700; letter-spacing: 0.1rem; margin-top: 2rem;";
+    resetBtn.textContent = "Play again?";
+    gameResult.appendChild(resetBtn);
+    reset();
+  } else if (computerScore === 5) {
+    gameResult.textContent = `HA, YOU'RE A LOSER! The computer is better than you.`;
+    resetBtn.style.cssText =
+      "background-color: #001845; color: #f8f8f8;text-transform: uppercase; padding: 0.5rem 1.6rem; border-radius: 0.5rem; cursor: pointer; font-weight: 700; letter-spacing: 0.1rem; margin-top: 2rem;";
+    resetBtn.textContent = "Play again?";
+    gameResult.appendChild(resetBtn);
+    reset();
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "Choose rock, paper, scissors, lizard or Spock:"
-    ).toLowerCase();
-    const computerSelection = computerPlay();
-    console.log(
-      `You played: ${playerSelection}, computer played: ${computerSelection}`
-    );
-    console.log(playRound(playerSelection, computerSelection));
-  }
-
-  if (playerScore > computerScore) {
-    console.log(
-      `Congratulation, you are a winner, you have bested the computer!`
-    );
-  } else if (playerScore < computerScore) {
-    console.log(`Sorry, you are a loser, computer has bested you!`);
-  } else {
-    console.log(`It's a tie!`);
-  }
+function reset() {
+  resetBtn.addEventListener("click", () => window.location.reload());
 }
-
-// game();
